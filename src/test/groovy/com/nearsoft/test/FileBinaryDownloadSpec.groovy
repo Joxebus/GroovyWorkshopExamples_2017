07150bf0f7ea713de718f9dbb65098ccb82c32f8
Omar Bautista <joxebus@gmail.com>
2017-03-10 17:21:49 -0700

    Setup spec to clean directory everytime it's executed

--- src/test/groovy/com/nearsoft/test/FileBinaryDownloadSpec.groovy
+++ src/test/groovy/com/nearsoft/test/FileBinaryDownloadSpec.groovy
@@ -8,6 +8,18 @@ import spock.lang.Specification;
  */
 public class FileBinaryDownloadSpec extends Specification{
 
+    def setupSpec(){
+        def src = new File('src/test/resources/tmp')
+        if(src.exists()){
+            src.eachFileRecurse {
+                it.delete()
+            }
+        }else{
+            src.mkdir()
+        }
+
+    }
+
     def "Test if a file can be downloaded"(){
         when:
         def fullName = "${filename}${Math.abs(new Random().nextInt() % 600 + 1)}.jpg"
